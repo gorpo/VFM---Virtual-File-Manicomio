8c01029a09f839cdbb93cf50a136ff6f7747e29a
GorpoOrko <47964672+gorpo@users.noreply.github.com>
2020-01-10 13:57:22 -0300

    Add files via upload

---
+++ vfm-admin/class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+
+ */
+$vfm_version = '3.4.6';
+
+require_once 'class/class.utils.php';
+
+require_once 'class/class.setup.php';
+
+require_once 'class/class.gatekeeper.php';
+
+require_once 'class/class.logger.php';
+
+require_once 'class/class.location.php';
+
+require_once 'class/class.actions.php';
+
+require_once 'class/class.downloader.php';
+
+require_once 'class/class.uploader.php';
+
+require_once 'class/class.updater.php';
+
+require_once 'class/class.resetter.php';
+
+require_once 'class/class.imageserver.php';
+
+require_once 'class/class.template.php';
